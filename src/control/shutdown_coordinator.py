baf8c74dcb08af1526ece393afaf700b86c8ad0c
ccathlete <ccathlete01@gmail.com>
2026-01-15 18:53:46 +0100

    Fixed a bug in the shutdown.

--- src/control/shutdown_coordinator.py
+++ src/control/shutdown_coordinator.py
@@ -1,20 +1,19 @@
 # src/control/shutdown_coordinator.py
 import asyncio
 import signal
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass(frozen=True)
 class ShutdownCoordinator:
-    stop_event = asyncio.Event()
+    stop_event: asyncio.Event = field(default_factory=asyncio.Event)
 
     def install_signal_handlers(self) -> None:
         loop = asyncio.get_event_loop()
-
         for sig in (signal.SIGINT, signal.SIGTERM):
-            loop.add_signal_handler(sig, self._on_signal, sig)
+            loop.add_signal_handler(sig, self._on_signal)
 
-    def _on_signal(self, sig: signal.Signals) -> None:
+    def _on_signal(self) -> None:
         self.stop_event.set()
 
     async def wait(self) -> None:
